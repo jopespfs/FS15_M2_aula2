edc7179e8adf6c1737983e85297e82b021f49611
jopespfs <juaumpedro2@gmail.com>
2023-01-11 09:59:13 -0300

    aula 2 - variaveis e constantes

---
+++ scripts.js
@@ -0,0 +1,45 @@
+// document.getElementById("container").innerHTML = "conteudo";
+// document.getElementById("container").innerHTML = "otra coisa";
+
+let container = document.getElementById("container");
+
+
+// ----------------------------------------
+
+// let macas = 2;
+// let bananas = 3;
+// let kiwi = 5;
+// let pitaya = 4;
+// let soma = Math.floor(macas * bananas + kiwi / pitaya);
+
+// container.innerHTML = soma;
+
+// ----------------------------------------
+ 
+// let texto = "Meu Nome é: ";
+// let nome = "João Pedro S P F Santos"
+
+// let soma = texto + nome;
+
+// container.innerHTML = soma;
+
+// ----------------------------------------
+
+// const NASCIMENTO = "30/07/1995";
+// let nome = "João Pedro S P F Santos"
+
+// container.innerHTML = nome + NASCIMENTO;
+
+// nome = "João Pedro Silveira";
+// NASCIMENTO = "31/07/96"
+
+// container.innerHTML = nome + NASCIMENTO;
+
+// ----------------------------------------
+
+function somar()
+{
+  let v1 = Number(valor1.value);
+  let v2 = Number(valor2.value);
+  document.getElementById("resultado").innerHTML = v1 + v2;
+}
